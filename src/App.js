07fa9759ff9ab882d342ff6f8905c90ed871548c
Sarath Kumar <nagarajsarath@gmail.com>
2019-09-22 12:17:20 -0400

    Add collection preview and collection item component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx'
+import ShopPage from './pages/shop/shop.component';
 
 
 const HatsPage = (props) => (
@@ -19,7 +20,7 @@ function App() {
     <div>
         <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route path='/hats' component={HatsPage} />
+          <Route path='/shop' component={ShopPage} />
         </Switch>
     </div>
   );
