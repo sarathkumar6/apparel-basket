c8de14f5f6154a014ab28ba507eaa286400509af
Sarath Kumar <nagarajsarath@gmail.com>
2019-11-18 21:48:25 -0500

    Update component names

--- src/pages/sign-in-sign-up/sign.in.sign.up.component.jsx
+++ src/pages/sign-in-sign-up/sign.in.sign.up.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './sign.in.sign.up.component.styles.scss';
-import SignIn from '../../components/sign-in/sign.in.component'
+import SignIn from '../../components/sign-in/sign.in.component';
+import SignUp from '../../components/sign-up/sign.up.component';
 
 const SignInSignUpPage = () => (
     <div className='sign-in-sign-up'> 
         <SignIn></SignIn>
+        <SignUp></SignUp>
     </div>
 )
 
