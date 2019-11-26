928e851068e365ba186a7b6bdac0b5a08b9d7461
Sarath Kumar <nagarajsarath@gmail.com>
2019-11-25 20:44:55 -0500

    Add cart item component and render the same in cart dropdown component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
-
+import _ from 'lodash';
 import ApparelBasketButton from '../apparel-basket-button/apparel.basket.button.component'
+
+import CartItem from '../cart-item/cart-item.component';
+import {connect} from 'react-redux';
 
 import './cart-dropdown.styles.scss';
 
 
-const CartDropDown = () => (
+const CartDropDown = ({cartItems}) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
+        {
+            _.map(cartItems, (item) => (
+                <CartItem key={item.id} item={item}></CartItem>
+            ))
+        }
         </div>
         <ApparelBasketButton>
             Go To Checkout
@@ -15,4 +23,7 @@ const CartDropDown = () => (
     </div>
 )
 
-export default CartDropDown;
+const mapStateToProps = ({cart: {cartItems}}) =>({
+    cartItems
+})
+export default connect(mapStateToProps, null)(CartDropDown);
